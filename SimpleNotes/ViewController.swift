4dc66b1c5308c4838911bfb9cc0a0f5e5a9f58dd
rsPogromishe <rs.pogromishe@gmail.com>
2023-01-12 11:59:23 +0400

    Initial Commit

---
+++ SimpleNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleNotes
+//
+//  Created by Снытин Ростислав on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
